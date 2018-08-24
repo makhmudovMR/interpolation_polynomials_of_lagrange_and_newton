7b8994140c2689031a0aadee9b72200ca25cffff
Magomed Makhmudov <mmriadovich@gmail.com>
2018-08-24 14:24:24 +0300

    Lagrange test derivetive

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from tkinter import *
 import scipy.interpolate
+import scipy.misc
 import numpy as np
 
 
@@ -70,15 +71,24 @@ class MyApp:
         # rN.grid(row=4, column=5, padx=10, pady=10)
 
         labelframe = LabelFrame(self.master, text="Ответы")
-        labelframe.grid(row=3, column=0, columnspan=4)
+        labelframe.grid(row=3, column=0, columnspan=5, padx=10, pady=10)
 
         self.label_L = StringVar()
+        self.label_deriv_L = StringVar()
         self.label_N = StringVar()
+        self.label_deriv_N = StringVar()
+        self.label_D = StringVar()
 
         Label(labelframe, textvariable=self.label_L).grid(row=0, column=0, padx=10, pady=10)
+        Label(labelframe, textvariable=self.label_deriv_L).grid(row=0, column=1, padx=10, pady=10)
         Label(labelframe, textvariable=self.label_N).grid(row=1, column=0, padx=10, pady=10)
+        Label(labelframe, textvariable=self.label_deriv_N).grid(row=1, column=1, padx=10, pady=10)
+        Label(labelframe, textvariable=self.label_D).grid(row=0, column=2, padx=10, pady=10)
         self.label_L.set("Лаградж")
+        self.label_deriv_L.set("производная Лагрнаджа")
         self.label_N.set("Ньютон")
+        self.label_deriv_N.set("производная Ньютона")
+        self.label_D.set("D = |L'(x) - N'(x)|")
 
     """load data from text fields"""
     def load_data(self):
@@ -105,7 +115,9 @@ class MyApp:
         self.text_y4.delete("1.0",END)
 
         self.label_L.set("Лаградж")
+        self.label_deriv_L.set("производная Лагрнаджа")
         self.label_N.set("Ньютон")
+        self.label_deriv_N.set("производная Ньютона")
 
     def calc(self):
         self.calc_L()
@@ -116,26 +128,42 @@ class MyApp:
         Y = [self.text_y1.get("1.0", END), self.text_y2.get("1.0", END), self.text_y3.get("1.0", END), self.text_y4.get("1.0", END)]
         X = list(map(lambda x: float(x[0:-1]), X))
         Y = list(map(lambda y: float(y[0:-1]), Y))
-        # print(X)
-        # print(Y)
 
         X = np.array(X)
         Y = np.array(Y)
 
-        """Lagrange formula in action"""
-
-        result = scipy.interpolate.lagrange(X,Y)
-        self.label_L.set(result)
-
         def sub_formula(x):
             return (-7.729*(x**3))+(1.35*(x**2)) + 2.859*x
 
-        print(result(0.2))
+        """Lagrange formula in action"""
+
+        result = scipy.interpolate.lagrange(X,Y)
+        self.label_L.set("L(x)= " + str(result))
+        self.label_deriv_L.set("L'(x) = "+str(scipy.misc.derivative(sub_formula, 0.2)))
+
+
+
 
 
     def calc_N(self):
-        pass
+        X = [self.text_x1.get("1.0", END),
+             self.text_x2.get("1.0", END),
+             self.text_x3.get("1.0", END),
+             self.text_x4.get("1.0", END)]
 
+        Y = [self.text_y1.get("1.0", END),
+             self.text_y2.get("1.0", END),
+             self.text_y3.get("1.0", END),
+             self.text_y4.get("1.0", END)]
+
+        X = list(map(lambda x: float(x[0:-1]), X))
+        Y = list(map(lambda y: float(y[0:-1]), Y))
+
+        X = np.array(X)
+        Y = np.array(Y)
+
+        # result = scipy.interpolate.newton()
+        # self.label_L.set(result)
 
 
 
